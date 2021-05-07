ab0cd35d4e389cf3361225ea6032b2fe440a204f
Mysticfox99 <ernestas.milinskas@gmail.com>
2021-05-07 02:18:01 +0100

    Settings Menu
    A settings menu has been implemented and a shadow to the skeleton has been added.

--- TSE Tower Def/Assets/Scripts/SceneManagerScript.cs
+++ TSE Tower Def/Assets/Scripts/SceneManagerScript.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    
+    Resolution[] resolutions; //Create array for the resolutions.
+    //Main Menu Code
     private void Start()
     {
+        resolutions = Screen.resolutions; //Gather list of resolutions available
+        resolutionDropdown.ClearOptions(); //Clear resolutions in option dropdown
+        
+        List<string> options = new List<string>(); //List of  strings containing resolutions
+
+        int currentResolutionIndex = 0; //Index used to make sure the selected resolution is default
+
+        //Loop through resolutions array 
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height; //Create a string for each resolution
+            options.Add(option); //Add the string to the options list
+
+            //Compare the resolution lenght and height to see if the correct resolution is being used
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height) 
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options); //Add options list to resolutions dropdown
+        resolutionDropdown.value = currentResolutionIndex; //Set the resolution to default
+        resolutionDropdown.RefreshShownValue(); //Display the resolution
+
         DontDestroyOnLoad(gameObject);
     }
     public void SwitchToScene(int input)
@@ -16,5 +47,31 @@ public class SceneManagerScript : MonoBehaviour
     public void ApplicationQuit()
     {
         Application.Quit();
+    }
+
+    //Settings Menu Code
+    //Volume Slider Settings
+
+    //Full screen toggle option
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    //Set the resolution of the game
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    //Set the quality of the project
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex); //Change the index based on the quality settings in the project settings
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
     }
 }
